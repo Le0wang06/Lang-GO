60e564e8c3f22f328e2c18ab4c2fd9cd4276aa9a
Le0wang06 <098leowang@gmail.com>
2025-05-27 15:17:36 -0400

    complete setup

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ from langchain_core.messages import HumanMessage, SystemMessage
 
 from langchain_core.prompts import ChatPromptTemplate
 
+from langchain.chat_models import init_chat_model
+
+#Ask for API key
 if not os.environ.get("OPENAI_API_KEY"):
   os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
 
-from langchain.chat_models import init_chat_model
-
+#Initialize model
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
-
-from langchain_core.prompts import ChatPromptTemplate
-
+#Define prompt template
 system_template = "Translate the following from English into {language}"
 
+
+#Define prompt
 prompt_template = ChatPromptTemplate.from_messages(
     [("system", system_template), ("user", "{text}")]
 )
+
+#Define prompt
 prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
 
-prompt.to_messages()
-
+#Print response
 response = model.invoke(prompt)
 print(response.content)

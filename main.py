71f1052dada6fb8bafadf78850f9a606aa1871b3
Le0wang06 <098leowang@gmail.com>
2025-05-27 15:11:32 -0400

    chatprompttemp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import getpass
 import os
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage, SystemMessage, ChatPromptTemplate
 
 
 if not os.environ.get("OPENAI_API_KEY"):
@@ -11,18 +11,10 @@ from langchain.chat_models import init_chat_model
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
 
-messages = [
-    SystemMessage("Translate the following from English into Italian"),
-    HumanMessage("hi!"),
-]
+from langchain_core.prompts import ChatPromptTemplate
 
-model.invoke("Hello")
+system_template = "Translate the following from English into {language}"
 
-model.invoke([{"role": "user", "content": "Hello"}])
-
-model.invoke([HumanMessage("Hello")])
-
-model.invoke(messages)
-
-for token in model.stream(messages):
-    print(token.content, end="|")
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)

6de9a0a193ae8e6b5503e20dfb1314aa3639df5f
Le0wang06 <098leowang@gmail.com>
2025-05-27 21:53:30 -0400

    use of prompt

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import getpass
 import os
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 from langchain.chat_models import init_chat_model
 
@@ -17,12 +17,22 @@ if not os.environ.get("OPENAI_API_KEY"):
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
 
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You talk like a pirate. Answer all questions to the best of your ability.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
 
 # Define a new graph
 workflow = StateGraph(state_schema=MessagesState)
 
 def call_model(state: MessagesState):
-    response = model.invoke(state["messages"])
+    prompt = prompt_template.invoke(state)
+    response = model.invoke(prompt)
     return {"messages": response}
 
 # Define the (single) node in the graph
@@ -34,17 +44,8 @@ memory = MemorySaver()
 app = workflow.compile(checkpointer=memory)
 
 
-model.invoke(
-    [
-        HumanMessage(content="Hi! I'm Bob"),
-        AIMessage(content="Hello Bob! How can I assist you today?"),
-        HumanMessage(content="What's my name?"),
-    ]
-)
-config = {"configurable": {"thread_id": "abc123"}}
-
-
-query = "What's my name?"
+config = {"configurable": {"thread_id": "abc345"}}
+query = "Hi! I'm Jim."
 
 input_messages = [HumanMessage(query)]
 output = app.invoke({"messages": input_messages}, config)

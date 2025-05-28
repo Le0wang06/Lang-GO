8ff46a18458489d0c65e3896100c1be4d61c959e
Le0wang06 <098leowang@gmail.com>
2025-05-28 13:20:28 -0400

    agent

--- agent.py
+++ agent.py
@@ -1,9 +1,17 @@
 # Import relevant functionality
+import getpass
+import os
 from langchain_anthropic import ChatAnthropic
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.messages import HumanMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
+
+# Set up API keys
+if not os.environ.get("TAVILY_API_KEY"):
+    os.environ["TAVILY_API_KEY"] = getpass.getpass("Enter your Tavily API key: ")
+if not os.environ.get("ANTHROPIC_API_KEY"):
+    os.environ["ANTHROPIC_API_KEY"] = getpass.getpass("Enter your Anthropic API key: ")
 
 # Create the agent
 memory = MemorySaver()
@@ -11,3 +19,12 @@ model = ChatAnthropic(model_name="claude-3-sonnet-20240229")
 search = TavilySearchResults(max_results=2)
 tools = [search]
 agent_executor = create_react_agent(model, tools, checkpointer=memory)
+
+
+config = {"configurable": {"thread_id": "abc123"}}
+for step in agent_executor.stream(
+    {"messages": [HumanMessage(content="hi im bob! and i live in sf")]},
+    config,
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
